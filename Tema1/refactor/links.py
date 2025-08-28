0ffbcbbc09775c78773c36470fcb01033643c37c
Freezerond <91515706+Freezerond@users.noreply.github.com>
2025-08-28 11:53:22 +0300

    inks.py

--- Tema1/refactor/links.py
+++ Tema1/refactor/links.py
@@ -4,11 +4,21 @@ from datetime import date
 from bs4 import BeautifulSoup
 
 
-def parse_page_links(html: str, start_date: date, end_date: date) -> List[Tuple[str, date]]: # убран параметр url, поскольку в функции он не используется
+def parse_page_links(html, start_date, end_date, url="https://spimex.com"):
     """
     Парсит ссылки на бюллетени с одной страницы:
     <a class="accordeon-inner__item-title link xls" href="/upload/reports/oil_xls/oil_xls_20240101_test.xls">link1</a>
+
+    Args:
+        html (str): HTML страницы.
+        start_date (date): Начальная дата диапазона.
+        end_date (date): Конечная дата диапазона.
+        url (str): Базовый URL для относительных ссылок.
+
+    Returns:
+        list[tuple[str, date]]: Список (ссылка, дата).
     """
+
     results = []
     soup = BeautifulSoup(html, "html.parser")
     links = soup.find_all("a", class_="accordeon-inner__item-title link xls")
@@ -25,12 +35,13 @@ def parse_page_links(html: str, start_date: date, end_date: date) -> List[Tuple[
         try:
             date_bulletin = href.split("oil_xls_")[1][:8] # название переменной изменено с date на date_bulletin, чтобы не перекрывать импортированный date из datetime
             file = datetime.datetime.strptime(date_bulletin, "%Y%m%d").date()
-            if start_date <= file <= end_date:
-                u = href if href.startswith("http") else f"https://spimex.com{href}"
-                results.append((u, file))
-            else:
-                print(f"Ссылка {href} вне диапазона дат")
         except Exception as e:
             print(f"Не удалось извлечь дату из ссылки {href}: {e}")
+            continue
 
+        if start_date <= file <= end_date:
+            u = href if href.startswith("http") else f"{url}{href}"
+            results.append((u, file))
+        # больше не выводим ссылки, которые нам не подошли
+        
     return results
