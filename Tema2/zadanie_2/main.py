bdc58cbf0f05ab6efa0bd3fd0ec0f9f73e9d47c9
Freezerond <91515706+Freezerond@users.noreply.github.com>
2025-09-16 17:27:37 +0300

    Update main.py

--- Tema2/zadanie_2/main.py
+++ Tema2/zadanie_2/main.py
@@ -6,8 +6,9 @@ from database.queries.orm import create_tables
 
 
 if __name__ == '__main__':
-    # asyncio.run(create_tables())
+    asyncio.run(create_tables())
     start = time.perf_counter()
     asyncio.run(main())
     end = time.perf_counter()
     print(f"Скрипт отработал за {end - start:.2f} секунд")
+
